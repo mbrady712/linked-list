891d38602266fe2d2ac8cdd09d08684c25c3b24f
Michael Brady <michaelbrady712@gmail.com>
2023-03-06 13:08:02 -0600

    Added prototype for the LinkedList class

--- linkedlist.h
+++ linkedlist.h
@@ -3,3 +3,27 @@ Name: Michael Brady
 Programmin Fundamentals III
 Purpose: This program contians a doubly Linked List ADT.
 **********************/
+#ifndef LINKED_LIST
+#define LINKED_LIST
+
+#include "data.h"
+
+class LinkedList {
+
+public:
+    LinkedList();
+    bool addNode(int, string*); 
+    bool deleteNode(int);
+    bool getNode(int, Data*);
+    void printList(bool = false); 
+    int getCount();
+    bool clearList();
+    bool exists(int);
+    ~LinkedList();
+
+private:
+    Node *head; 
+
+};
+
+#endif //LINKED_LIST
