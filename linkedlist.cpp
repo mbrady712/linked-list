281ca89a40c42870f1ec703bfc4d328e4d3cfbc6
Michael Brady <michaelbrady712@gmail.com>
2023-03-06 13:20:20 -0600

    Added stubs for methods of LinkedList class.

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -3,3 +3,41 @@ Name: Michael Brady
 Programmin Fundamentals III
 Purpose: This program contians a doubly Linked List ADT.
 **********************/
+
+#include "linkedlist.h"
+
+LinkedList::LinkedList(){
+
+}
+
+bool LinkedList::addNode(int id, string* data){
+    return true;
+}
+
+bool LinkedList::deleteNode(int id){
+    return true;
+}
+
+bool LinkedList::getNode(int id, Data* data){
+    return true;
+}
+
+void LinkedList::printList(bool){
+
+}
+
+int LinkedList::getCount(){
+    return 1;
+}
+
+bool LinkedList::clearList(){
+    return true;
+}
+
+bool LinkedList::exists(int id){
+    return true;
+}
+
+LinkedList::~LinkedList(){
+
+}
